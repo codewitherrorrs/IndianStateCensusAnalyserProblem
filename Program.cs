7cbff5f61d07586c36e2bcf23176230f973eb7e9
“codewitherrors” <codewitherrors1@gmail.com>
2023-03-12 11:33:24 +0530

    Add : Welcome Message to the Indian State Census Analyser Problem

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace IndianStateCensusAnalyserProblem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Indian State Census Analyser Problems");
+        }
+    }
+}
